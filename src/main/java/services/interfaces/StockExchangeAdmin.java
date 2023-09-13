ed40cedca3b84550373a65b9c5cd48c647e02876
Hugo Alves Dutra <40148665+hugo-dutra@users.noreply.github.com>
2023-09-13 13:57:49 -0300

    feat:added admin resources

--- src/main/java/services/interfaces/StockExchangeAdmin.java
+++ src/main/java/services/interfaces/StockExchangeAdmin.java
@@ -2,8 +2,11 @@ package services.interfaces;
 
 import models.Asset;
 
+import java.util.List;
+
 public interface StockExchangeAdmin {
     void addAsset(Asset asset);
     void removeAsset(Asset asset);
     void updateAsset(Asset asset);
+    List<Asset> getAssets();
 }
--- src/main/java/subjects/interfaces/StockExchange.java
+++ src/main/java/subjects/interfaces/StockExchange.java
@@ -3,12 +3,16 @@ package subjects.interfaces;
 import models.Asset;
 import observers.interfaces.StockBroker;
 
+import java.util.List;
+
 public interface StockExchange {
     void addAsset(Asset asset);
     void removeAsset(Asset asset);
     void updateAsset(Asset asset);
 
+    List<Asset> getAssets();
+
     void addStockBroker(StockBroker stockBroker);
     void removeStockBroker(StockBroker stockBroker);
-    void notifyStockBrokers();
+    void notifyStockBrokers(Asset asset);
 }
