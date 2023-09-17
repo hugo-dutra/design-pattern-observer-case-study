559a675a2fb1d5a455b449bb2357d9a9c93e393d
Hugo Alves Dutra <40148665+hugo-dutra@users.noreply.github.com>
2023-09-17 14:03:42 -0300

    feat: implemented logger chain warning

--- src/main/java/chainOfResponsability/logger/ClientChainOfResponsability.java
+++ src/main/java/chainOfResponsability/logger/ClientChainOfResponsability.java
@@ -1,30 +1,35 @@
-package chainOfResponsability;
+package chainOfResponsability.logger;
 
 import chainOfResponsability.logger.implementations.LoggerHandlerDebug;
 import chainOfResponsability.logger.implementations.LoggerHandlerInfo;
+import chainOfResponsability.logger.implementations.LoggerHandlerWarning;
 import chainOfResponsability.logger.interfaces.LoggerHandler;
 
 public class ClientChainOfResponsability {
     public static void main(String[] args) {
         LoggerHandler loggerHandler = new LoggerHandlerInfo();
         LoggerHandler loggerHandlerDebug = new LoggerHandlerDebug();
-        LoggerHandler loggerHandlerWarning = new LoggerHandlerDebug();
+        LoggerHandler loggerHandlerWarning = new LoggerHandlerWarning();
 
         loggerHandler.setNextLoggerHandler(loggerHandlerDebug);
         loggerHandlerDebug.setNextLoggerHandler(loggerHandlerWarning);
 
 
+        System.out.println("==================================================");
         loggerHandler.logMessage("This is an info message", chainOfResponsability.logger.enums.LogLevel.INFO, true);
         loggerHandler.logMessage("This is another info message", chainOfResponsability.logger.enums.LogLevel.INFO, true);
-        System.out.println("--------------------------------------------------");
-        loggerHandler.logMessage("This is a debug message", chainOfResponsability.logger.enums.LogLevel.DEBUG, true);
-        loggerHandler.logMessage("This is another debug message", chainOfResponsability.logger.enums.LogLevel.DEBUG, true);
-        System.out.println("--------------------------------------------------");
-        loggerHandler.logMessage("This is a warning message", chainOfResponsability.logger.enums.LogLevel.WARNING, true);
-        loggerHandler.logMessage("This is another warning message", chainOfResponsability.logger.enums.LogLevel.WARNING, true);
-        loggerHandler.logMessage("This is another warning message too", chainOfResponsability.logger.enums.LogLevel.WARNING, true);
+
         System.out.println("--------------------------------------------------");
 
+        loggerHandler.logMessage("This is a debug message", chainOfResponsability.logger.enums.LogLevel.DEBUG, false);
+        loggerHandler.logMessage("This is another debug message", chainOfResponsability.logger.enums.LogLevel.DEBUG, true);
+
+        System.out.println("--------------------------------------------------");
+
+        loggerHandler.logMessage("This is a warning message", chainOfResponsability.logger.enums.LogLevel.WARNING, false);
+        loggerHandler.logMessage("This is another warning message", chainOfResponsability.logger.enums.LogLevel.WARNING, false);
+        loggerHandler.logMessage("This is another warning message too", chainOfResponsability.logger.enums.LogLevel.WARNING, true);
+        System.out.println("==================================================");
 
 
 
