497a1c394ecacd51f388531d8a697f0d938ab5f7
Hugo Alves Dutra <40148665+hugo-dutra@users.noreply.github.com>
2023-09-17 14:07:39 -0300

    feat: added handlerError to chain

--- src/main/java/chainOfResponsability/logger/ClientChainOfResponsability.java
+++ src/main/java/chainOfResponsability/logger/ClientChainOfResponsability.java
@@ -1,6 +1,7 @@
 package chainOfResponsability.logger;
 
 import chainOfResponsability.logger.implementations.LoggerHandlerDebug;
+import chainOfResponsability.logger.implementations.LoggerHandlerError;
 import chainOfResponsability.logger.implementations.LoggerHandlerInfo;
 import chainOfResponsability.logger.implementations.LoggerHandlerWarning;
 import chainOfResponsability.logger.interfaces.LoggerHandler;
@@ -10,9 +11,11 @@ public class ClientChainOfResponsability {
         LoggerHandler loggerHandler = new LoggerHandlerInfo();
         LoggerHandler loggerHandlerDebug = new LoggerHandlerDebug();
         LoggerHandler loggerHandlerWarning = new LoggerHandlerWarning();
+        LoggerHandler loggerHandlerError = new LoggerHandlerError();
 
         loggerHandler.setNextLoggerHandler(loggerHandlerDebug);
         loggerHandlerDebug.setNextLoggerHandler(loggerHandlerWarning);
+        loggerHandlerWarning.setNextLoggerHandler(loggerHandlerError);
 
 
         System.out.println("==================================================");
@@ -29,11 +32,14 @@ public class ClientChainOfResponsability {
         loggerHandler.logMessage("This is a warning message", chainOfResponsability.logger.enums.LogLevel.WARNING, false);
         loggerHandler.logMessage("This is another warning message", chainOfResponsability.logger.enums.LogLevel.WARNING, false);
         loggerHandler.logMessage("This is another warning message too", chainOfResponsability.logger.enums.LogLevel.WARNING, true);
+
+        System.out.println("--------------------------------------------------");
+
+        loggerHandler.logMessage("This is an error message", chainOfResponsability.logger.enums.LogLevel.ERROR, false);
+        loggerHandler.logMessage("This is another error message", chainOfResponsability.logger.enums.LogLevel.ERROR, false);
+        loggerHandler.logMessage("This is another error message too", chainOfResponsability.logger.enums.LogLevel.ERROR, true);
+
         System.out.println("==================================================");
-
-
-
-
 
     }
 }
---
+++ src/main/java/chainOfResponsability/logger/implementations/LoggerHandlerError.java
@@ -0,0 +1,34 @@
+package chainOfResponsability.logger.implementations;
+
+import chainOfResponsability.logger.enums.LogLevel;
+import chainOfResponsability.logger.interfaces.LoggerHandler;
+
+public class LoggerHandlerError implements LoggerHandler {
+    private LoggerHandler nextLoggerHandler;
+    private String logMessage;
+    @Override
+    public void setNextLoggerHandler(LoggerHandler nextLoggerHandler) {
+        this.nextLoggerHandler = nextLoggerHandler;
+    }
+
+    @Override
+    public void logMessage(String message, LogLevel logLevel, boolean showLogMessage) {
+        if(logLevel == LogLevel.ERROR) {
+            this.logMessage = "ERROR: " + message;
+            this.sendEmailWithMessageToAdmin();
+            if (showLogMessage) {
+                this.showLogMessage();
+            }
+        } else {
+            this.nextLoggerHandler.logMessage(message, logLevel, showLogMessage);
+        }
+    }
+
+    private void showLogMessage() {
+        System.out.println(this.logMessage);
+    }
+
+    private void sendEmailWithMessageToAdmin() {
+        System.out.println("Sending email with message: " + this.logMessage);
+    }
+}
