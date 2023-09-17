e4ddc24586dc8aa9033d1f5be1a64dd5a7d4eafc
Hugo Alves Dutra <40148665+hugo-dutra@users.noreply.github.com>
2023-09-17 13:22:00 -0300

    feat: main method initial implementation

--- src/main/java/chainOfResponsability/ClientChainOfResponsability.java
+++ src/main/java/chainOfResponsability/ClientChainOfResponsability.java
@@ -1,7 +1,22 @@
 package chainOfResponsability;
 
+import chainOfResponsability.logger.implementations.LoggerHandlerDebug;
+import chainOfResponsability.logger.implementations.LoggerHandlerInfo;
+import chainOfResponsability.logger.interfaces.LoggerHandler;
+
 public class ClientChainOfResponsability {
     public static void main(String[] args) {
-        System.out.println("Chain of Responsability Pattern\n");
+        LoggerHandler loggerHandler = new LoggerHandlerInfo();
+        LoggerHandler loggerHandlerDebug = new LoggerHandlerDebug();
+        loggerHandler.setNextLoggerHandler(loggerHandlerDebug);
+
+
+        loggerHandler.logMessage("This is an info message", chainOfResponsability.logger.enums.LogLevel.INFO);
+        loggerHandler.logMessage("This is another info message", chainOfResponsability.logger.enums.LogLevel.INFO);
+
+
+        loggerHandler.logMessage("This is a debug message", chainOfResponsability.logger.enums.LogLevel.DEBUG);
+        loggerHandler.logMessage("This is another debug message", chainOfResponsability.logger.enums.LogLevel.DEBUG);
+
     }
 }
